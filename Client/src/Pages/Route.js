b7a900e626c2617fa2fa4751b781a7ff9a72c125
Soniya0001 <soniyasigroha001@gmail.com>
2024-04-23 21:44:14 +0530

    Update origin

--- Client/src/Pages/Route.js
+++ Client/src/Pages/Route.js
@@ -4,8 +4,7 @@ import Filter from './Filter';
 import Details from './Details'
 import Header from './Header';
 import { useEffect, useState } from 'react';
-
-// const BASE_URL = window.env.REACT_APP_BASE_URL;
+const BASE_URL = window.env.REACT_APP_BASE_URL;
 
 const Router = () => {
 
@@ -13,7 +12,7 @@ const Router = () => {
 
     useEffect(() => {
         const getUser = () => {
-            fetch('http://localhost:5500/auth/login/success', {
+            fetch(`${BASE_URL}/auth/login/success`, {
                 method: "GET",
                 credentials: "include",
                 headers: {
