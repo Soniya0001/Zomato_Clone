f169f3375397173d4b121d017db6baeb53baa1ee
Soniya0001 <soniyasigroha001@gmail.com>
2024-05-16 12:02:32 +0530

    Update Home.js base_url

--- Client/src/Pages/Home.js
+++ Client/src/Pages/Home.js
@@ -3,7 +3,22 @@ import '../Styles/frontpage.css';
 import Banner from './Banner';
 import QuickSearch from './QuickSearch';
 import axios from 'axios';  //help in connecting to API's
+import Modal from 'react-modal';
 const BASE_URL = window.env.REACT_APP_BASE_URL;
+
+const customStyles = {
+    overlay:{
+        backgroundColor: "rgba(0,0,0,0.8)"
+    },
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+    },
+};
 
 class Homepage extends React.Component {
 
@@ -18,7 +33,7 @@ class Homepage extends React.Component {
   componentDidMount(){
     //location API
     axios({
-        url: `${BASE_URL}/location`,
+         url: `${BASE_URL}/location`,
         method: 'get',
         headers: {'Content-Type': 'application/JSON'}
     })
